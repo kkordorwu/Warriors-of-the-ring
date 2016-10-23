89b44c52817a0f8f477b8b73239c9eddc39027d3
rythmk <rythmk@yahoo.com>
2016-10-22 18:54:56 -0700

    commit

---
+++ FightersController/fightersControllers.js
@@ -0,0 +1,69 @@
+angular.module("fighters")
+    .controller("fightersControllers", function ($scope) {
+
+        $scope.fighters = [{
+            "firstName": "Diego",
+            "lastName": "Corrales",
+            "homeTown": "Sacramento usa",
+            "weight": 174,
+            "stance": "Orthodox",
+            "wins": "40",
+            "loses": "5"
+        }, {
+            "firstName": "Bernard",
+            "lastName": "Hopkins",
+            "homeTown": "Philadelphia usa",
+            "weight": 174,
+            "stance": "Orthodox",
+            "wins": "40",
+            "loses": "5"
+        }, {
+            "firstName": "Juan",
+            "lastName": "Marquez",
+            "homeTown": "Mexico",
+            "weight": 174,
+            "stance": "Orthodox",
+            "wins": "40",
+            "loses": "5"
+        }, {
+            "firstName": "Mike",
+            "lastName": "Tyson",
+            "homeTown": "new York city",
+            "weight": 174,
+            "stance": "Orthodox",
+            "wins": "40",
+            "loses": "5"
+        }, {
+            "firstName": "Buakaw",
+            "lastName": "Banchamek",
+            "homeTown": "thaiLand",
+            "weight": 174,
+            "stance": "Orthodox",
+            "wins": "40",
+            "loses": "5"
+        }, {
+            "firstName": "Saenchai",
+            "homeTown": "thaiLand",
+            "lastName": "PK",
+            "weight": 174,
+            "stance": "Orthodox",
+            "wins": "40",
+            "loses": "5"
+        }, {
+            "firstName": "Chris",
+            "lastName": "Eubank",
+            "homeTown": "Dulwich",
+            "weight": 174,
+            "stance": "Orthodox",
+            "wins": "40",
+            "loses": "5"
+        }, {
+            "firstName": "Masato",
+            "lastName": "Kobayashi",
+            "homeTown": "Japan",
+            "weight": 174,
+            "stance": "Orthodox",
+            "wins": "40",
+            "loses": "5"
+        }]
+    });
