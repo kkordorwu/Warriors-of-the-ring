f1ffb36d60221d2b58391833724100be2b52ee42
rythmk <rythmk@yahoo.com>
2016-10-23 13:14:06 -0700

    commit

--- FightersController/fightersControllers.js
+++ FightersController/fightersControllers.js
@@ -10,8 +10,8 @@ angular.module("fighters")
             "homeTown": "Sacramento usa",
             "weight": 174,
             "stance": "Orthodox",
-            "wins": "40",
-            "loses": "5",
+            "wins": 40,
+            "loses": 5,
             "pictureLink" : "http://static.boxrec.com/thumb/f/f8/DiegoCorrales.jpg/280px-DiegoCorrales.jpg"
         }, {
             "firstName": "Bernard",
@@ -19,55 +19,55 @@ angular.module("fighters")
             "homeTown": "Philadelphia usa",
             "weight": 174,
             "stance": "Orthodox",
-            "wins": "40",
-            "loses": "5"
+            "wins": 40,
+            "loses": 5
         }, {
             "firstName": "Juan",
             "lastName": "Marquez",
             "homeTown": "Mexico",
             "weight": 174,
             "stance": "Orthodox",
-            "wins": "40",
-            "loses": "5"
+            "wins": 40,
+            "loses": 5
         }, {
             "firstName": "Mike",
             "lastName": "Tyson",
             "homeTown": "new York city",
             "weight": 174,
             "stance": "Orthodox",
-            "wins": "40",
-            "loses": "5"
+            "wins": 40,
+            "loses": 5
         }, {
             "firstName": "Buakaw",
             "lastName": "Banchamek",
             "homeTown": "thaiLand",
             "weight": 174,
             "stance": "Orthodox",
-            "wins": "40",
-            "loses": "5"
+            "wins": 40,
+            "loses": 5
         }, {
             "firstName": "Saenchai",
             "homeTown": "thaiLand",
             "lastName": "PK",
             "weight": 174,
             "stance": "Orthodox",
-            "wins": "40",
-            "loses": "5"
+            "wins": 40,
+            "loses": 5
         }, {
             "firstName": "Chris",
             "lastName": "Eubank",
             "homeTown": "Dulwich",
             "weight": 174,
             "stance": "Orthodox",
-            "wins": "40",
-            "loses": "5"
+            "wins": 40,
+            "loses": 5
         }, {
             "firstName": "Masato",
             "lastName": "Kobayashi",
             "homeTown": "Japan",
             "weight": 174,
             "stance": "Orthodox",
-            "wins": "40",
-            "loses": "5"
+            "wins": 40,
+            "loses": 5
         }]
     });
